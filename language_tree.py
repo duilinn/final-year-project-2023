bd533686109f1af7b8bcb657f8c0697e3ec1256a
David King <duilinn2001@gmail.com>
2023-02-11 11:22:32 +0000

    Latest version of files before working on project today

--- language_tree.py
+++ language_tree.py
@@ -1,9 +1,5 @@
 import re
 
-father_path = "C:\\Users\\david\\TCD\\4th year\\final year project\\daughter_etymology.txt"
-father_file = open(father_path, encoding='utf-8')
-father_etymology = father_file.read()
-
 languages_list_path = "C:\\Users\\david\\TCD\\4th year\\final year project\\languages_list.txt"
 languages_list_file = open(languages_list_path, encoding='utf-8')
 languages_list = languages_list_file.read().split('\n')
@@ -60,156 +56,182 @@ class Tree:
                 for x in self.children:
                     x.add_word(language, word, level+1)
                     
-    def print_tree(self, current_level=0, lc=[]):
+    def print_tree(self, current_level=0, lc=[],force_show = False):
 
-        for i in range(current_level - 1):
-            if (lc[i]): print("     ",end="")
-            else: print("│    ",end="")
+        #print this node
 
-        print("├───",end="")
+        if (self.word !="" or force_show):
+            for i in range(current_level - 1):
+                if (lc[i]): print("     ",end="")
+                else: print("│    ",end="")
+
+            print("├───",end="")
             #else: print("├───",end="")
         
-        print(self.language,end="")# + " " + self.word)
-        if (self.word != ""): print(": " + self.word,end="")
-        #print(".".join((["O" if x else "." for x in lc])))
-        print()
-        #print("self.get_children = {0}".format(len(self.get_children())))
+            print(self.language,end="")# + " " + self.word)
+            if (self.word != ""): print(": " + self.word,end="")
+            #print(".".join((["O" if x else "." for x in lc])))
+            print()
+            #print("self.get_children = {0}".format(len(self.get_children())))
+
+        #print children
 
         number_of_children = len(self.get_children())
 
         for x in range(len(self.get_children())):
             new_lc = lc[:]
             new_lc.append(x == (number_of_children-1))
-            self.get_children()[x].print_tree(current_level+1, new_lc)
+            self.get_children()[x].print_tree(current_level+1, new_lc, force_show)
                 
 
+while(True):
+    #choose English word to get its etymology
+    chosen_word = input("Enter an English word: ")
+
+    word_path = "C:\\Users\\david\\TCD\\4th year\\final year project\\" + \
+        chosen_word + ".txt"
+    word_file = open(word_path, encoding='utf-8')
+    item = word_file.read()
+
+    ie_tree = Tree("Proto-Indo-European",coords=[49.4, 31.2])
+    ie_tree.add_child("Proto-Germanic", "Proto-Indo-European",coords=[54.9, 9.2])
+
+    ie_tree.add_child("Old Frisian", "Proto-Germanic",coords=[53.35, 6.80])
+    ie_tree.add_child("West Frisian", "Old Frisian",coords=[53.14, 5.86])
+    ie_tree.add_child("Old Dutch", "Proto-Germanic",coords=[52.16, 5.20])
+    ie_tree.add_child("Middle Dutch", "Old Dutch",coords=[51.66, 5.34])
+    ie_tree.add_child("Dutch", "Middle Dutch", coords=[52.00, 5.00])
+    ie_tree.add_child("Old Saxon", "Proto-Germanic",coords=[52.37, 9.72])
+    ie_tree.add_child("Middle Low German", "Old Saxon",coords=[53.14, 9.67])
+    ie_tree.add_child("Old High German","Proto-Germanic",coords=[49.8,10.0])
+    ie_tree.add_child("Middle High German", "Old High German",coords=[50.4,11.7])
+    ie_tree.add_child("German", "Middle High German")
+
+    ie_tree.add_child("early Scandinavian", "Proto-Germanic")
+    ie_tree.add_child("Old Icelandic","early Scandinavian")
+    ie_tree.add_child("Norn", "early Scandinavian")
+    ie_tree.add_child("Old Swedish", "early Scandinavian")
+    ie_tree.add_child("Swedish", "Old Swedish")
+    ie_tree.add_child("Old Danish", "early Scandinavian")
+    ie_tree.add_child("Danish", "Old Danish")
+
+    ie_tree.add_child("Gothic", "Proto-Germanic")
+
+    ie_tree.add_child("Proto-Indo-Iranian", "Proto-Indo-European")
+    ie_tree.add_child("Sanskrit", "Proto-Indo-Iranian")
+    ie_tree.add_child("Old Avestan", "Proto-Indo-Iranian")
+    ie_tree.add_child("Younger Avestan", "Proto-Indo-Iranian")
+
+    ie_tree.add_child("ancient Greek", "Proto-Indo-European")
+
+    ie_tree.add_child("classical Latin", "Proto-Indo-European")
+    ie_tree.add_child("Old French","classical Latin")
+    ie_tree.add_child("Middle French","Old French")
+    ie_tree.add_child("Old Occitan","classical Latin")
+    ie_tree.add_child("Occitan","Old Occitan")
+    ie_tree.add_child("Catalan","classical Latin")
+    ie_tree.add_child("Spanish","classical Latin")
+    ie_tree.add_child("Portuguese","classical Latin")
+    ie_tree.add_child("Italian","classical Latin")
+
+    ie_tree.add_child("Oscan", "Proto-Indo-European")
+
+    ie_tree.add_child("Proto-Celtic", "Proto-Indo-European")
+    ie_tree.add_child("Gaulish","Proto-Celtic")
+    ie_tree.add_child("Early Irish","Proto-Celtic")
+    ie_tree.add_child("Irish","Early Irish")
+    ie_tree.add_child("Welsh","Proto-Celtic")
+
+    ie_tree.add_child("Armenian","Proto-Indo-European")
+
+    ie_tree.add_child("Tocharian A","Proto-Indo-European")
+    ie_tree.add_child("Tocharian B","Proto-Indo-European")
+
+    ie_tree.add_child("Early Runic","Proto-Germanic")
+
+    ie_tree.add_child("Mycenaean Greek","Proto-Indo-European")
+
+    ie_tree.add_child("Proto-Balto-Slavic","Proto-Indo-European")
+    ie_tree.add_child("Proto-Baltic","Proto-Balto-Slavic")
+    ie_tree.add_child("Old Prussian","Proto-Baltic")
+    ie_tree.add_child("Lithuanian","Proto-Baltic")
+
+    ie_tree.add_child("Proto-Slavic","Proto-Balto-Slavic")
+    ie_tree.add_child("Old Church Slavonic","Proto-Slavic")
+
+    ie_tree.print_tree(0, [False], force_show=True)
+
+    language_words_pairs = []
+
+    #parse lines according to
+    #LANGUAGE_NAME - WORD_INFO - LANGUAGE NAME- WORD_INFO
 
 
-ie_tree = Tree("Proto-Indo-European",coords=[49.4, 31.2])
-ie_tree.add_child("Proto-Germanic", "Proto-Indo-European",coords=[54.9, 9.2])
-
-ie_tree.add_child("Old Frisian", "Proto-Germanic",coords=[53.35, 6.80])
-ie_tree.add_child("West Frisian", "Old Frisian",coords=[53.14, 5.86])
-ie_tree.add_child("Old Dutch", "Proto-Germanic",coords=[52.16, 5.20])
-ie_tree.add_child("Middle Dutch", "Old Dutch",coords=[51.66, 5.34])
-ie_tree.add_child("Dutch", "Middle Dutch", coords=[52.00, 5.00])
-ie_tree.add_child("Old Saxon", "Proto-Germanic",coords=[52.37, 9.72])
-ie_tree.add_child("Middle Low German", "Old Saxon",coords=[53.14, 9.67])
-ie_tree.add_child("Old High German","Proto-Germanic",coords=[49.8,10.0])
-ie_tree.add_child("Middle High German", "Old High German",coords=[50.4,11.7])
-ie_tree.add_child("German", "Middle High German")
-
-ie_tree.add_child("early Scandinavian", "Proto-Germanic")
-ie_tree.add_child("Old Icelandic","early Scandinavian")
-ie_tree.add_child("Norn", "early Scandinavian")
-ie_tree.add_child("Old Swedish", "early Scandinavian")
-ie_tree.add_child("Swedish", "Old Swedish")
-ie_tree.add_child("Old Danish", "early Scandinavian")
-ie_tree.add_child("Danish", "Old Danish")
-
-ie_tree.add_child("Gothic", "Proto-Germanic")
-
-ie_tree.add_child("Proto-Indo-Iranian", "Proto-Indo-European")
-ie_tree.add_child("Sanskrit", "Proto-Indo-Iranian")
-ie_tree.add_child("Old Avestan", "Proto-Indo-Iranian")
-ie_tree.add_child("Younger Avestan", "Proto-Indo-Iranian")
-
-ie_tree.add_child("ancient Greek", "Proto-Indo-European")
-
-ie_tree.add_child("classical Latin", "Proto-Indo-European")
-ie_tree.add_child("Old French","classical Latin")
-ie_tree.add_child("Middle French","Old French")
-ie_tree.add_child("Old Occitan","classical Latin")
-ie_tree.add_child("Occitan","Old Occitan")
-ie_tree.add_child("Catalan","classical Latin")
-ie_tree.add_child("Spanish","classical Latin")
-ie_tree.add_child("Portuguese","classical Latin")
-ie_tree.add_child("Italian","classical Latin")
-
-ie_tree.add_child("Oscan", "Proto-Indo-European")
-
-ie_tree.add_child("Proto-Celtic", "Proto-Indo-European")
-ie_tree.add_child("Gaulish","Proto-Celtic")
-ie_tree.add_child("Early Irish","Proto-Celtic")
-
-ie_tree.add_child("Armenian","Proto-Indo-European")
-
-ie_tree.add_child("Tocharian A","Proto-Indo-European")
-ie_tree.add_child("Tocharian B","Proto-Indo-European")
-
-ie_tree.print_tree(0, [False])
-
-language_words_pairs = []
-
-#parse lines according to
-#LANGUAGE_NAME - WORD_INFO - LANGUAGE NAME- WORD_INFO
+    language_vs_word_positions = ""
+    x = 0
 
 
-language_vs_word_positions = ""
-x = 0
-item = father_etymology
+
+    #locate language-words pairs
+
+    while (x < len(item)):
+        lang_found = False
+        
+        for language in languages_list:
+            #if item.find(language) == x:
+            indices = [index for index in range(len(item)) if item.startswith(language, index)]
+            if x in indices:
+                lang_found = True
+                lang_length = len(language)
+
+                for j in range(lang_length):
+                    language_vs_word_positions += "l"
+                    x += 1
+            #else:
+                #print("item: \n {0} \n language: {1}\nx: {2}\nitem.find(language): {3}".format(item,language,x,item.find(language)))
+        if (not lang_found):
+            language_vs_word_positions += "w"
+            x += 1
+
+    #print("item:\n{0}\n\nlvw:\n{1}\n".format(item, language_vs_word_positions))
+    #break up each language-words pair
+
+    j = 0
+
+    punctuation = "<>().:;"
+    while (j < len(item)):
+        #print("j = {0}".format(j))
+        current_language = ""
+        current_words = ""
+
+        while (language_vs_word_positions[j] == "l"):
+            if (item[j] not in punctuation): current_language += item[j]
+            j += 1
+
+        while (j < len(item) and language_vs_word_positions[j] == "w"):
+            if (item[j] not in punctuation): current_words += item[j]
+            j += 1
+
+        current_language = current_language.strip()
+        current_words = current_words.strip()
+        
+        if len(current_words) > 0 and current_words[-1] == ",": current_words = current_words[:-1]
+
+        #fix nordic languages
 
 
-#locate language-words pairs
+        #print("current_language, current_words = " + current_language + " | " + current_words + "\n")
+        language_words_pairs.append([current_language,current_words])
 
-while (x < len(item)):
-    lang_found = False
-    
-    for language in languages_list:
-        #if item.find(language) == x:
-        indices = [index for index in range(len(item)) if item.startswith(language, index)]
-        if x in indices:
-            lang_found = True
-            lang_length = len(language)
+    language_words_pairs = [[x.strip(),y.strip()] for [x,y] in language_words_pairs]
 
-            for j in range(lang_length):
-                language_vs_word_positions += "l"
-                x += 1
-        #else:
-            #print("item: \n {0} \n language: {1}\nx: {2}\nitem.find(language): {3}".format(item,language,x,item.find(language)))
-    if (not lang_found):
-        language_vs_word_positions += "w"
-        x += 1
+    #add words to language tree
+    for [language_name, words] in language_words_pairs:
+        ie_tree.add_word(language_name, words)
 
-#print("item:\n{0}\n\nlvw:\n{1}\n".format(item, language_vs_word_positions))
-#break up each language-words pair
+    print ("\n#####################################\n")
 
-j = 0
-
-punctuation = "<>().:;"
-while (j < len(item)):
-    #print("j = {0}".format(j))
-    current_language = ""
-    current_words = ""
-
-    while (language_vs_word_positions[j] == "l"):
-        if (item[j] not in punctuation): current_language += item[j]
-        j += 1
-
-    while (j < len(item) and language_vs_word_positions[j] == "w"):
-        if (item[j] not in punctuation): current_words += item[j]
-        j += 1
-
-    current_language = current_language.strip()
-    current_words = current_words.strip()
-    
-    if current_words[-1] == ",": current_words = current_words[:-1]
-
-    #fix nordic languages
-
-
-    #print("current_language, current_words = " + current_language + " | " + current_words + "\n")
-    language_words_pairs.append([current_language,current_words])
-
-language_words_pairs = [[x.strip(),y.strip()] for [x,y] in language_words_pairs]
-
-#add words to language tree
-for [language_name, words] in language_words_pairs:
-    ie_tree.add_word(language_name, words)
-
-print ("\n#####################################\n")
-
-ie_tree.print_tree()
+    ie_tree.print_tree(force_show=False)
 
 """
 Etymology: Cognate with Old Frisian fader , feder (West Frisian
@@ -232,5 +254,4 @@ probably originally a derivative (with suffixation) of a nursery word
 of the pa type (see papa n.2). A similar suffix is found in other
 Indo-European relationship terms (compare mother n.1, brother n.,
 daughter n.), although these suffixes may ultimately be of different
-origin.
-"""
+origin."""
